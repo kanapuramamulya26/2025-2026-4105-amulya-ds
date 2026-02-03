134cf3b6296208e65fed23dc493a6ab35d5c64ad
kanapuramamulya26 <amulyakanpuram26@gmail.com>
2026-02-03 14:40:36 +0530

    Created using Colab

---
+++ list.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOzkOv5ifKEm6Gqp/Z8jfk+",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/kanapuramamulya26/2025-2026-4105-amulya-ds/blob/main/list.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "8qM6hStuuVU0",
+        "outputId": "a89d6839-0f79-4765-ae99-ebbc6d7bd058"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "[1, 2, 3, 4, 5]\n"
+          ]
+        }
+      ],
+      "source": [
+        "list=[1,2,3,4,5]\n",
+        "print(list)"
+      ]
+    }
+  ]
+}
